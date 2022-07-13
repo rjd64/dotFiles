a884a499872a98c893dec04c47debe3e72bf5d44
rjd64 <86621417+rjd64@users.noreply.github.com>
2022-07-13 09:41:21 -0400

    updated .fils for neovim (added telescope and changed some plugins to work w it)

---
+++ nvim/lua/nvim-telescope.lua
@@ -0,0 +1,4 @@
+local nvim_telescope = require("telescope")
+nvim_telescope.setup{}
+nvim_telescope.load_extension("file_browser")
+nvim_telescope.load_extension("ui-select")
--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -2,9 +2,12 @@
 -- Parsers must be installed manually via :TSInstall
 
 require('nvim-treesitter.configs').setup {
+ -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
   highlight = {
     -- false will disable the whole extension
-    enable = true, 
+    enable = true,
     disable = {},
     additional_vim_regex_highlighting = false,
   },
@@ -18,25 +21,32 @@ require('nvim-treesitter.configs').setup {
     },
   },
   indent = {
-    enable = true,
+    enable = false,
   },
   textobjects = {
     select = {
       enable = true,
       -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true, 
+      lookahead = true,
       keymaps = {
         -- You can use the capture groups defined in textobjects.scm
         ['af'] = '@function.outer',
         ['if'] = '@function.inner',
         ['ac'] = '@class.outer',
         ['ic'] = '@class.inner',
+        -- 'a' for argument
+        ['aa'] = '@parameter.outer',
+        ['ia'] = '@parameter.inner',
+        ['aC'] = '@conditional.outter',
+        ['iC'] = '@conditional.inner',
+        ['aL'] = '@loop.outer',
+        ['iL'] = '@loop.inner',
       },
     },
     move = {
       enable = true,
       -- whether to set jumps in the jumplist
-      set_jumps = true, 
+      set_jumps = true,
       goto_next_start = {
         [']m'] = '@function.outer',
         [']]'] = '@class.outer',
@@ -55,5 +65,23 @@ require('nvim-treesitter.configs').setup {
       },
     },
   },
+  refactor = {
+    smart_rename = {
+      enable = true,
+      keymaps = {
+        smart_rename = 'grr',
+      },
+      navigation = {
+        enable = true,
+        keymaps = {
+          list_definitions = 'glD',
+          list_definition_toc = 'g0',
+          goto_next_usage = '<a-*>',
+          goto_previous_usage = '<a-#>',
+       },
+     },
+    },
+  },
 }
 
+require'treesitter-context'.setup{}
